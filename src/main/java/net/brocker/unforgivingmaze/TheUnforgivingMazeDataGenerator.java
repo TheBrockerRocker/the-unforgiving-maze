2e0d5a56e5d04dd27a62c0b180983f10e46efe0f
BuildTools <unconfigured@null.spigotmc.org>
2024-12-05 21:50:41 +0100

    forgot to add them to datagen class

--- src/main/java/net/brocker/unforgivingmaze/TheUnforgivingMazeDataGenerator.java
+++ src/main/java/net/brocker/unforgivingmaze/TheUnforgivingMazeDataGenerator.java
@@ -1,11 +1,18 @@
 package net.brocker.unforgivingmaze;
 
+import net.brocker.unforgivingmaze.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class TheUnforgivingMazeDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModBlockLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
