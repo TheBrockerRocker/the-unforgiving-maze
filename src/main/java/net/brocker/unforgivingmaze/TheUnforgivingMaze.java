5bb6602ef81c833178b4c9cbb6232834e9d0ed49
BuildTools <unconfigured@null.spigotmc.org>
2024-12-05 17:28:41 +0100

    added item helper class

--- src/main/java/net/brocker/unforgivingmaze/TheUnforgivingMaze.java
+++ src/main/java/net/brocker/unforgivingmaze/TheUnforgivingMaze.java
@@ -1,5 +1,6 @@
 package net.brocker.unforgivingmaze;
 
+import net.brocker.unforgivingmaze.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class TheUnforgivingMaze implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/brocker/unforgivingmaze/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.brocker.unforgivingmaze.item;
+
+import net.brocker.unforgivingmaze.TheUnforgivingMaze;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TheUnforgivingMaze.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TheUnforgivingMaze.LOGGER.info("registering mod items for " + TheUnforgivingMaze.MOD_ID);
+    }
+}
