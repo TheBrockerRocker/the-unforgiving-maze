6e8f6b07523f2585086e29d25d76723fd3165e02
SCsupercraft <alisonandant2020@gmail.com>
2024-12-07 20:13:52 +0000

    Add config

--- src/main/java/net/brocker/unforgivingmaze/TheUnforgivingMaze.java
+++ src/main/java/net/brocker/unforgivingmaze/TheUnforgivingMaze.java
@@ -1,6 +1,7 @@
 package net.brocker.unforgivingmaze;
 
 import net.brocker.unforgivingmaze.block.ModBlocks;
+import net.brocker.unforgivingmaze.config.ModConfig;
 import net.brocker.unforgivingmaze.item.ModItemGroups;
 import net.brocker.unforgivingmaze.item.ModItems;
 import net.fabricmc.api.ModInitializer;
@@ -14,9 +15,14 @@ public class TheUnforgivingMaze implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModConfig.initialize();
+
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 
 		ModItemGroups.registerItemGroups();
+
+		// Save config on game close
+		Runtime.getRuntime().addShutdownHook(new Thread(ModConfig.HANDLER::save));
 	}
 }
